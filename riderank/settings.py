29d12236138b803d03f7092c109f2e975e764b2b
Amir <amir@gmail.com>
2026-02-03 22:38:45 -0800

    Add Swagger documentation for API

--- riderank/settings.py
+++ riderank/settings.py
@@ -38,8 +38,21 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'drf_spectacular',
     'core',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'RideRank API',
+    'DESCRIPTION': 'Trust-based ride rating system (Trip, Rating, Report, ScoreEvent)',
+    'VERSION': '1.0.0',
+    'SERVE_INCLUDE_SCHEMA': False,
+}
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
